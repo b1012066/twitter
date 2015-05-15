779330ff60af74aa4d69d451413763f2e33b9c76
osanai <b1012066@fun.ac.jp>
2015-05-15 16:50:18 +0900

    競合起きるかな？

--- test.php
+++ test.php
@@ -6,6 +6,7 @@
 	<h1>こんな感じ<h1>
 	<br>
 	<h1><?php echo("phpで出力したよ～ん"); ?><h1>
-	<h2><?php echo("git pushできたよ〜ん");?><h2>
+	    <h2><?php echo("git pushできたよ〜ん");?><h2>
+		<h2>kokokokoko</h2>
 </body>
 </html>
