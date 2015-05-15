d23cfbd594d8389de560507dea6679d8bdb771ec
b1012206 <b1012206@fun.ac.jp>
2015-05-15 16:49:34 +0900

    phpinfoを表示する

--- test.php
+++ test.php
@@ -1,11 +1,1 @@
-<html>
-<head>
-	<title>TESTだよん</title>
-</head>
-<body>
-	<h1>こんな感じ<h1>
-	<br>
-	<h1><?php echo("phpで出力したよ～ん"); ?><h1>
-	<h2><?php echo("git pushできたよ〜ん");?><h2>
-</body>
-</html>
+<?php phpinfo(); ?>
